05684736ba2045e7ae227cc661e6f99a9c394e3d
Abd El-Rahman Nabil <a.nabiil@hotmail.com>
2026-01-19 05:21:25 +0200

    fx

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'https://dev.haddaf.net/dashboard/admin';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'https://pulse.test/admin';
 const token = document.head.querySelector('meta[name="csrf-token"]');
 if (token) {
     axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
---
+++ routes/admin.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register admin routes for your application.
+|
+*/
+
+// Add your admin routes here

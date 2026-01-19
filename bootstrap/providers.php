aed1c15d367c66d8e9e8b5171e9c8ec540c7886f
Abd El-Rahman Nabil <a.nabiil@hotmail.com>
2026-01-19 05:05:50 +0200

    fx

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,9 +1,4 @@
 <?php
 
 return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\FirebaseServiceProvider::class,
-    App\Providers\HorizonServiceProvider::class,
-    App\Providers\SocialiteServiceProvider::class,
-    Maatwebsite\Excel\ExcelServiceProvider::class,
 ];
